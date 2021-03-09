267b6d49d12bed74aad074a5dc1d7f0900e2e375
Zach Hornung <Zachhornung@comcast.net>
2021-03-09 12:06:41 -0800

    added all routes except for paulos robot routes

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', homeGet);
-app.post('/robot', robotPost);
+app.post('/robotPost', robotPost);
+app.get('/seeRobot', getSeeRobot);
 app.post('/spotifyPlaylistResults', getSpotifyPlaylistResults);
+app.get('/popular', getFavorites);
+app.get('/aboutUs', getAboutUs);
 
 function homeGet(req, res){
-  console.log(req);
+  // console.log(req.body);
   res.render('pages/index.ejs');
 }
 function robotPost(req, res){
@@ -25,13 +28,24 @@ function robotPost(req, res){
   res.render('pages/zachRobot.ejs', {emotion: `${req.body.emotion}`});
 }
 function getSpotifyPlaylistResults(req, res){
-  console.log(req.body);
   // the output from the robot will be in the req.body.
   // put spotify api call here and pass the results into the res.render as the second argument
 
 
   // spotify api call^^^^^^^^^
   res.render('pages/playlists.ejs', {emotion:req.body.emotionFromRobot});
+}
+function getFavorites(req, res){
+  // console.log(req.body);
+  // right here is where the sql query for getting popular playlists goes
+  // send the favorites into the render function
+  res.render('pages/popular.ejs');
+}
+function getAboutUs(req, res){
+  res.render('pages/aboutUs');
+}
+function getSeeRobot(req, res){
+  res.render('pages/seeRobot');
 }
 
 
