d315e978ad24a8311965e55d4af5699473c72d4f
Zach Hornung <Zachhornung@comcast.net>
2021-03-12 10:18:05 -0800

    got rid of lorem ipsum

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ passport.use(
     {
       clientID: client_id,
       clientSecret: client_secret,
-      callbackURL: redirect_uri + PORT + authCallbackPath,
+      callbackURL: redirect_uri + authCallbackPath,
 
     },
     // from; https://github.com/JMPerez/passport-spotify/blob/master/examples/login/app.js
